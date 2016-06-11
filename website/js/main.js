9110257203f478e08764f61ab28660eece9c1b96
Andrew Bonifacio <hello@andrewbdesign.com>
2016-06-11 23:33:49 +1000

    Mobile structure

---
+++ website/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  //Load page partials
+  $("#nav").load("views/nav.html")
+  $("#contact-form").load("views/contact.html")
+  $("#footer").load("views/footer.html")
+  var body = document.querySelector("body")
+  body.style.display = "block"
+
+  setTimeout(function(){
+    $("#menu-hamburger").on("click", function(){
+      $("#menu-list").toggleClass("hide")
+    })
+  },250)
+
+
+
+
+
+
+
+})
